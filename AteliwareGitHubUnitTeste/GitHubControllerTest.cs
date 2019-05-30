0442b254042bbfb5464fd1d2c7b4f53159f5ca93
Thales <thalescampelo@gmail.com>
2019-05-30 19:16:50 -0300

    adicionando o projeto de teste unitário.

---
+++ AteliwareGitHubUnitTeste/GitHubControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using AteliwareGitHub2.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AteliwareGitHubUnitTeste
+{
+    [TestClass]
+    public class GitHubControllerTest
+    {
+        [TestMethod]
+        public void IndexView()
+        {
+            var controller = new GitHubController();
+            var result = controller.Index() as ActionResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DetalhesView()
+        {
+            var controller = new GitHubController();
+            var result = controller.Detalhes(1) as ActionResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
